df77db0cc7ace3156e8bdeb513a49c3e2da72011
chez <chester-vyacheslav@yandex.ru>
2022-12-15 20:18:31 +0300

    Cosmetic changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# anime-character-visualization
-Mini-app for visualizing anime character by description. Homework of Programming Practice course of Saint-Petersburg State University.
+# Anime character visualization
+This mini-app can generate picture of person by description in style of given anime title.
+
+The application is developed as homework for programming practice course of Saint-Petersburg State University 
+
+## Access
+1. Streamlit web application via [link](https://anime-character-visualization.streamlit.app/)
+2. Telegram bot (IN PROGRESS)
--- api.py
+++ api.py
@@ -4,10 +4,10 @@ import json
 
 
 def visualize_query(description: str, title: str) -> requests.Response:
-    __HF_API_TOKEN = os.getenv("HF_API_TOKEN")
-    __MODEL_NAME = "DGSpitzer/Cyberpunk-Anime-Diffusion"
-    __API_URL = f"https://api-inference.huggingface.co/models/{__MODEL_NAME}"
-    headers = {"Authorization": f"Bearer {__HF_API_TOKEN}"}
+    hf_api_token = os.getenv("HF_API_TOKEN")
+    model_name = "DGSpitzer/Cyberpunk-Anime-Diffusion"
+    api_url = f"https://api-inference.huggingface.co/models/{model_name}"
+    headers = {"Authorization": f"Bearer {hf_api_token}"}
     text = f"{description} in {title} world"
     data = json.dumps(text)
-    return requests.request("POST", __API_URL, headers=headers, data=data)
+    return requests.request("POST", api_url, headers=headers, data=data)
